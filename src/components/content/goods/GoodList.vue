2dd7d5489b683b2c90daa91edd5145aaf7026304
徐伟(SSC) <xuwei9@cofcoko.com>
2020-12-11 17:30:22 +0800

    '2020-12-11-02'

---
+++ src/components/content/goods/GoodList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="(item, iid) in goods" :key="iid" :goods-item="item"/>
+  </div>
+</template>
+
+<script>
+  import GoodsListItem from "./GoodsListItem";
+
+  export default {
+    name: "GoodList",
+    components: {
+      GoodsListItem
+    },
+    props: {
+      goods: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+    padding: 2px;
+  }
+</style>

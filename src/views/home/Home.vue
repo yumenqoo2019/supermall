ed736438b8c1636e827dbf2d7582db749bee3c66
徐伟(SSC) <xuwei9@cofcoko.com>
2020-12-11 09:51:14 +0800

    '2020-12-11'

---
+++ src/views/home/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommand-view :recommends="recommends"/>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommandView from "./childComps/RecommandView";
+
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommandView
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() {
+      // 1.请求多个数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
---
+++ src/views/home/childComps/RecommandView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommand">
+    <div v-for="(item, id) in recommends" :key="id" class="recommand-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RecommandView",
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return [];
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommand {
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 10px 0 10px;
+
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommand-item {
+    flex: 1;
+  }
+
+  .recommand img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>

687c5ae6f78d02514f94cb49b20d33041d2de2b8
徐伟(SSC) <xuwei9@cofcoko.com>
2020-12-16 17:17:52 +0800

    '2020-12-16'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- ref绑定组件 -->
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "Scroll",
+    data() {
+      return {
+        scroll: null,
+        message: '哈哈哈哈'
+      }
+    },
+    mounted() {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+
+      })
+      this.scroll.scrollTo(0, 0)
+
+    },
+    methods: {
+      scrollTo(x, y, time=300) {
+        this.scroll.scrollTo(x, y, time)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,76 +1,18 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
-    <recommand-view :recommends="recommends"/>
-    <feature-view/>
-    <tab-control class="tab-control"
-                 :titles="['流行', '新款', '精选']"
-                 @tabClick="tabClick"/>
-    <goods-list :goods="showGoods"></goods-list>
 
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
-      <li>11</li>
-      <li>12</li>
-      <li>13</li>
-      <li>14</li>
-      <li>15</li>
-      <li>16</li>
-      <li>17</li>
-      <li>18</li>
-      <li>19</li>
-      <li>20</li>
-      <li>21</li>
-      <li>22</li>
-      <li>23</li>
-      <li>24</li>
-      <li>25</li>
-      <li>26</li>
-      <li>27</li>
-      <li>28</li>
-      <li>29</li>
-      <li>30</li>
-      <li>31</li>
-      <li>32</li>
-      <li>33</li>
-      <li>34</li>
-      <li>35</li>
-      <li>36</li>
-      <li>37</li>
-      <li>38</li>
-      <li>39</li>
-      <li>40</li>
-      <li>41</li>
-      <li>42</li>
-      <li>43</li>
-      <li>44</li>
-      <li>45</li>
-      <li>46</li>
-      <li>47</li>
-      <li>48</li>
-      <li>49</li>
-      <li>50</li>
-      <li>51</li>
-      <li>52</li>
-      <li>53</li>
-      <li>54</li>
-      <li>55</li>
-      <li>56</li>
-      <li>57</li>
-      <li>58</li>
-      <li>59</li>
-      <li>60</li>
-    </ul>
+    <scroll class="content" ref="scroll">
+      <home-swiper :banners="banners"/>
+      <recommand-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control class="tab-control"
+                   :titles="['流行', '新款', '精选']"
+                   @tabClick="tabClick"/>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+
+    <back-top @click.native="backClick"/>
   </div>
 </template>
 
@@ -82,6 +24,8 @@
   import NavBar from "components/common/navbar/NavBar";
   import TabControl from "components/content/tabControl/TabControl";
   import GoodsList from "components/content/goods/GoodList";
+  import Scroll from "components/common/scroll/Scroll";
+  import BackTop from "components/content/backTop/BackTop";
 
   import {getHomeMultidata, getHomeGoods} from "network/home";
 
@@ -93,7 +37,9 @@
       FeatureView,
       NavBar,
       TabControl,
-      GoodsList
+      GoodsList,
+      Scroll,
+      BackTop
     },
     data() {
       return {
@@ -138,6 +84,11 @@
             break
         }
       },
+      // 组件原生监听事件
+      backClick() {
+        // this.$refs可以拿到scroll组件，拿到组件后可以直接调用组件的方法和属性
+        this.$refs.scroll.scrollTo(0, 0)
+      },
 
       /**
        * 网络请求相关方法
@@ -161,7 +112,10 @@
 
 <style scoped>
     #home {
-      padding-top: 40px;
+      /*padding-top: 44px;*/
+      /* vh viewport(视口) height*/
+      height: 100vh;
+      position: relative;
     }
 
   .home-nav {
@@ -176,7 +130,19 @@
   }
 
   .tab-control {
-    position: sticky;
+    /*position: sticky;*/
     top: 44px;
+    z-index: 9;
   }
+
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
 </style>
